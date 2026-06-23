5b4c14428a844d94b3fc76173e4c934881349b11
TarikEloukili <tarikeloukili03@email.com>
2026-06-23 12:34:37 +0100

    finished routes files

---
+++ src/routes/data.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+import os
+from models import ResponseSignal
+from helpers.config import get_settings, Settings
+from controllers import DataController, ProjectController
+import aiofiles
+import logging 
+
+logger = logging.getLogger("uvicorn.error")
+
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"],
+)
+
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
+
+    # validate file properties:
+    data_controller = DataController()
+    is_valid, result_signal = data_controller.validate_uploaded_file(file)
+
+    if not is_valid:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={
+            "result_signal": result_signal
+        })
+
+    
+    project_dir_path = ProjectController().get_project_path(project_id)
+
+    file_path, file_id = data_controller.generate_unique_file_path(file.filename, project_id)
+
+    try:
+
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+
+    except Exception as e:
+        logger.error(f"Error uploading file: {e}")
+        
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={
+            "result_signal": ResponseSignal.FILE_UPLOAD_FAILED.value,
+        })
+
+        
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, 
+        content={
+            "result_signal": ResponseSignal.FILE_UPLOADED_SUCCESS.value,
+            "file_id": file_id,
+        }
+    )
+
