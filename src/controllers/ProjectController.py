685c2a79f807153f9eefa00a610e8f36b75f34ed
TarikEloukili <tarikeloukili03@email.com>
2026-06-23 12:35:11 +0100

    finished controllers files

---
+++ src/controllers/ProjectController.py
@@ -0,0 +1,17 @@
+from controllers.BaseController import BaseController
+from fastapi import UploadFile
+from models import ResponseSignal
+import os
+
+class ProjectController(BaseController):
+    
+    def __init__(self):
+        super().__init__()
+
+    def get_project_path(self, project_id: str):
+        self.project_dir = os.path.join(self.files_dir, project_id)
+
+        if not os.path.exists(self.project_dir):
+            os.makedirs(self.project_dir)
+
+        return self.project_dir
